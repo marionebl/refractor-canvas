0710ffafdf3d92c96a3e51f5da2ee8aa8437a955
Mario Nebl <mnebl@atlassian.com>
2021-01-24 20:19:23 +1100

    feat: apply font styles

--- src/refractor-canvas.ts
+++ src/refractor-canvas.ts
@@ -13,8 +13,8 @@ export function refractorCanvas(
     height: number;
   }
 ) {
-  context.font = `${theme.fontSize}px ` + theme.text.fontFamily;
-  context.fillStyle = 'red';
+  context.font = `${theme.fontSize}px ${theme.text.fontFamily}`;
+  context.fillStyle = theme.text.color;
   context.textBaseline = "middle";
 
   tokens.reduce(chunk, []).reduce<Options>(render, {
@@ -74,14 +74,28 @@ function render(options: Options, token: RefractorNode) {
       token.properties.className[0] === "token"
         ? token.properties.className[1]
         : undefined;
+
     const style = options.theme.tokens[tokenName] || {};
+    const fontStyle = style.fontStyle ?? 'normal';
+    const fontWeight = style.fontWeight ?? 'normal';
+    const fontSize = options.theme.fontSize ?? 10;
+    const fontFamily = options.theme.text.fontFamily ?? 'sans-serif';
 
     options.context.fillStyle = style.color;
+    options.context.font = `${fontStyle} ${fontWeight} ${fontSize}px ${fontFamily}`;
+
     const childrenOptions = token.children.reduce<Options>(render, options);
 
-    options.context.font = options.theme.text.fontFamily;
+    options.context.font = `${options.theme.fontSize}px ${options.theme.text.fontFamily}`;
     options.context.fillStyle = options.theme.text.color;
 
-    return childrenOptions;
+    return {
+      line: childrenOptions.line,
+      char: childrenOptions.char,
+      charWidth: options.charWidth,
+      lineHeight: options.lineHeight,
+      context: options.context,
+      theme: options.theme,
+    };
   }
 }
